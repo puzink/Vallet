0ef3263c9f4d0b08605d414a3b09298b50960862
puzink <puzin95@mail.ru>
2024-07-19 21:58:04 +0300

    test containers

--- src/main/java/org/wallet/controllers/WalletController.java
+++ src/main/java/org/wallet/controllers/WalletController.java
@@ -1,19 +1,29 @@
 package org.wallet.controllers;
 
 import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.wallet.model.PerformActionRequest;
 import org.wallet.model.PerformActionResponse;
+import org.wallet.services.WalletService;
 
 @RestController
+@RequiredArgsConstructor
 public class WalletController {
+
+    private final WalletService walletService;
 
     @PostMapping("/action")
     public PerformActionResponse performAction(@RequestBody @Valid PerformActionRequest request) {
 
-        return null;
+        long amount = switch (request.getAction()) {
+            case WITHDRAW -> walletService.withdraw(request.getUuid(), request.getAmount());
+            case DEPOSIT -> walletService.deposit(request.getUuid(), request.getAmount());
+        };
+
+        return new PerformActionResponse(request.getUuid(), amount);
     }
 
 
---
+++ src/main/java/org/wallet/services/WalletService.java
@@ -0,0 +1,17 @@
+package org.wallet.services;
+
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Service
+public class WalletService {
+
+    public long withdraw(UUID uuid, long amount) {
+        return 0;
+    }
+
+    public long deposit(UUID uuid, long amount) {
+        return 0;
+    }
+}
